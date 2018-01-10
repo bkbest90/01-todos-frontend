d6932515e58b894292e94f1395cb4807ad7587d5
bkbest90 <wthnfbest@gmail.com>
2018-01-10 21:25:41 +0700

    Challenge 1&2 passed

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,21 +1,26 @@
 <template>
   <div>
-    <div v-for="todo in todos" :key="todo.title">
+    <div v-for="(todo, index) in todos" :key="todo.title">
       <b-field class="is-pulled-left">
-        <b-checkbox size="is-large">{{ todo.title }}</b-checkbox>
+        <b-checkbox size="is-large" @input="changeStat({index, completed: $event})"><strike v-if="todo.completed">{{ todo.title }}</strike>
+        <div v-else>{{ todo.title }}</div>
+        </b-checkbox>
       </b-field>
-      <a class="delete is-pulled-right"></a>
+      <a class="delete is-pulled-right" @click="removeTodo(index)"></a>
       <div class="is-clearfix"></div>
     </div>
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import { mapGetters, mapActions } from 'vuex'
 
 export default {
   computed: {
     ...mapGetters(['todos'])
+  },
+  methods: {
+    ...mapActions(['removeTodo', 'changeStat'])
   }
 }
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -7,7 +7,8 @@ export const store = new Vuex.Store({
   strict: true,
   state: {
     todos: [],
-    visibility: 'all'
+    visibility: 'all',
+    completed: false
   },
   mutations: {
     ADD_TODO (state, title) {
@@ -18,6 +19,12 @@ export const store = new Vuex.Store({
     },
     CHANGE_VISIBILITY (state, newVisibilityValue) {
       state.visibility = newVisibilityValue
+    },
+    REMOVE_TODO (state, index) {
+      state.todos.splice(index, 1)
+    },
+    CHANG_COMPLETE_STAT (state, {index, completed}) {
+      state.todos[index].completed = completed
     }
   },
   actions: {
@@ -26,6 +33,12 @@ export const store = new Vuex.Store({
     },
     changeVisibility ({commit}, newVisibilityValue) {
       commit('CHANGE_VISIBILITY', newVisibilityValue)
+    },
+    removeTodo ({commit}, index) {
+      commit('REMOVE_TODO', index)
+    },
+    changeStat ({commit}, {index, completed}) {
+      commit('CHANG_COMPLETE_STAT', {index, completed})
     }
   },
   getters: {
